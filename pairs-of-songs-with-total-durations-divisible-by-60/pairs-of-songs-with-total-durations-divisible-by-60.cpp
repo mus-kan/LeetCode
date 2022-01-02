4afb10a8fa84d228d7ed88cc6d910e721e49bd23
mus-kan <51403122+mus-kan@users.noreply.github.com>
2022-01-02 13:24:16 +0530

    Time: 96 ms (5.38%), Space: 23 MB (90.11%) - LeetHub

---
+++ pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numPairsDivisibleBy60(vector<int>& time) {
+        unordered_map<int,int>mp;
+        int ans=0;
+        
+        for(int i=0; i<time.size(); i++)
+        {
+           if(mp.find((600-time[i])%60)!=mp.end())
+               ans+=mp[(600-time[i])%60];
+            
+            mp[time[i]%60]++;
+        }
+        
+        return ans;
+        
+    }
+};
