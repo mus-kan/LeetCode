9e8746e6c3793ec2cdca2e7b7415ed5afa38fd59
mus-kan <51403122+mus-kan@users.noreply.github.com>
2022-01-19 17:44:29 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (42.68%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
- int numDecodings(string s) {
+  int numDecodings(string s) {
         int n = s.size();
-        vector<int> mem(n+1,-1);
-        mem[n]=1;
-        return s.empty()? 0 : num(0,s,mem);   
+        vector<int> dp(n+1);
+        dp[n] = 1;
+        for(int i=n-1;i>=0;i--) {
+            if(s[i]=='0') dp[i]=0;
+            else {
+                dp[i] = dp[i+1];
+                if(i<n-1 && (s[i]=='1'||s[i]=='2'&&s[i+1]<'7')) dp[i]+=dp[i+2];
+            }
+        }
+        return s.empty()? 0 : dp[0];   
     }
-    int num(int i, string &s, vector<int> &mem) {
-        if(mem[i]>-1) return mem[i];
-        if(s[i]=='0') return mem[i] = 0;
-        int res = num(i+1,s,mem);
-        if(i<s.size()-1 && (s[i]=='1'||s[i]=='2'&&s[i+1]<'7')) res+=num(i+2,s,mem);
-        return mem[i] = res;
-    }
+
 };
